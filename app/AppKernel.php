6e0cda5423d86218983a985531c4c5ff28239778
Giancarlo Ventura <sakya_stelios@hotmail.com>
2013-12-20 11:17:16 -0500

    Instalado FOSUserBundle y TinyMCE

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Sakya\GensBundle\GensBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Stfalcon\Bundle\TinymceBundle\StfalconTinymceBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
