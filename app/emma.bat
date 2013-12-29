fc780124c3d91dee50d7520d26b105f954135fbd
Emma Sakya <holas001@latinmail.com>
2013-12-29 15:33:00 -0500

    Inicialized my config

--- app/emma.bat
+++
@@ -1,2 +0,0 @@
-git config --global user.name "Emma Sakya"
-git config --global user.email holas001@latinmail.com
---
+++ init.bat
@@ -0,0 +1,4 @@
+git config --global user.name "Emma Sakya"
+git config --global user.email holas001@latinmail.com
+git fetch upstream
+git merge upstream/master
